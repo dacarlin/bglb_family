d07321749343f9410f7e51d97ae8bb0ea03712bf
Alex Carlin <dacarlin@ucdavis.edu>
2017-06-08 18:57:54 -0700

    Finished updating full auto dock DMS

--- full_auto_dock_dms/input_pkg/get_target.py
+++ full_auto_dock_dms/input_pkg/get_target.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 import os 
 
-FASTA='/share/work/alex/bglb_family/full_auto/targets.fa'
+FASTA='/share/work/alex/bglb_family/full_auto_dock_dms/targets.fa'
 task = os.getenv('SLURM_ARRAY_TASK_ID')
 task = int(task)
 for n, r in enumerate(SeqIO.parse(FASTA, 'fasta')):
