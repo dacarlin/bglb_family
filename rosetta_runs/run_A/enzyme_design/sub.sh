75ec11cee0a22d7346d1875c1945058d57493ce1
Alex Carlin <dacarlin@ucdavis.edu>
2017-06-08 18:21:06 -0700

    Updated notes

---
+++ rosetta_runs/run_A/enzyme_design/sub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# parallelize over mutant 
+
+#SBATCH --time 0-12:00
+#SBATCH -J runA
+#SBATCH -o logs/slurm_%A-%a.txt 
+#SBATCH --array=1-8026
+#SBATCH -p gc128 
+
+rsync -avz input_pkg/ output_files/$SLURM_ARRAY_TASK_ID/
+cd $_
+awk "NR==$SLURM_ARRAY_TASK_ID" list.txt > mut_flags 
+
+module load rosetta 
+rosetta_scripts.linuxgccrelease @flags @mut_flags
