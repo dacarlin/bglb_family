a4524cadc421c0bc9b541c5d9f657c04cb86d3e4
Alex Carlin <dacarlin@ucdavis.edu>
2017-06-25 01:04:42 -0700

    Added pipeline scripts and input files

---
+++ pipeline/assess_output.py
@@ -0,0 +1,33 @@
+from Bio import SeqIO 
+import pandas 
+import os 
+
+# here we go boys 
+
+def get_features():
+    for n in range(1, 2889):
+
+        # let's keep it simple. 
+        # did we get a docked model here? 
+
+        features = []
+        base = '/share/work/alex/bglb_family/completed_pipeline/output_files/'
+        r = next(SeqIO.parse(os.path.join(base, str(n), 'target.fasta'), 'fasta'))
+        name = r.id
+
+        if os.path.isfile(os.path.join(base, str(n), 'input_pose_0001.pdb')):
+            features.append(1)
+        else:
+            features.append(0)
+
+        sf = os.path.join(base, str(n), 'score.sc')
+        if os.path.isfile(sf):
+            df = pandas.read_csv(sf, sep='\s+')
+            df = df.sort_values('total_score').head(1)
+        
+        yield (name, *features) 
+
+df = pandas.DataFrame(get_features())
+print(df.head())
+print(df.shape) 
+df.to_hdf('results.h5', 'my_key') 
---
+++ pipeline/clean_up.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -r output_files/* logs/* 
