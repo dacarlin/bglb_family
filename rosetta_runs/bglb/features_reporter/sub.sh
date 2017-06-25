a830618b93ac12d182527d5ecb3e2aa3e165a5ca
Alex Carlin <dacarlin@ucdavis.edu>
2017-06-25 01:01:33 -0700

    Improvements to BglB runs

--- rosetta_runs/bglb/features_reporter/sub.sh
+++ rosetta_runs/bglb/features_reporter/sub.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# 
 #SBATCH -t 0-24:00
-#SBATCH -J feats 
+#SBATCH -J feat_bgl
 #SBATCH -o log.txt 
 #SBATCH --mem-per-cpu=16G 
-
 module load rosetta 
 rosetta_scripts.linuxgccrelease @flags 
-
--- rosetta_runs/run_A/enzyme_design/sub.sh
+++ rosetta_runs/run_A/enzyme_design/sub.sh
@@ -3,8 +3,7 @@
 #SBATCH --time 0-12:00
 #SBATCH -J runA
 #SBATCH -o logs/slurm_%A-%a.txt 
-#SBATCH --array=1-8
-##SBATCH --array=1-8092
+#SBATCH --array=1-8092
 #SBATCH -p gc128 
 
 rsync -avz input_pkg/ output_files/$SLURM_ARRAY_TASK_ID/
