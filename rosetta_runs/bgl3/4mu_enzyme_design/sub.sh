f9ec8e954ad0e20fbb19891e7326c1fafbb34b6f
Alex Carlin <carlin@ucdavis.edu>
2017-08-12 16:33:12 -0700

    Parallelized 4me enzyme design

--- rosetta_runs/bgl3/4mu_enzyme_design/sub.sh
+++ rosetta_runs/bgl3/4mu_enzyme_design/sub.sh
@@ -2,15 +2,19 @@
 #
 #SBATCH --time 0-12:00
 #SBATCH -J 4mu 
-#SBATCH --array=1-9580
 #SBATCH -p gc128 
 #SBATCH -o logs/slurm.%A-%a.out
+#SBATCH --array=1-9580
+
+module load alex-rosetta 
 
 hostname -f 
 rsync -avz input_pkg/ output_files/${SLURM_ARRAY_TASK_ID}
 cd $_
 pwd
-module load rosetta 
+
+tail -n $SLURM_ARRAY_TASK_ID list.txt | head -1 > mutant_flags 
+
 rosetta_scripts.linuxgccrelease @flags @mutant_flags 
 
 #~/Applications/main/source/bin/rosetta_scripts.default.macosclangrelease @flags @mutant_flags
